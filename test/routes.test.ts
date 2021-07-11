4302660f18cdf36843cd9183e6b010fcf8c2a8ec
Felix Haus <felixhaus@gmail.com>
2021-07-11 19:43:23 +0200

    Replace host header with x-forwarded-host

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -29,6 +29,7 @@ interface ProbeFile {
     status?: number;
     statusDescription?: string;
     responseHeaders?: Record<string, string>;
+    requestHeaders?: Record<string, string>;
     destPath?: string;
   }[];
 }
@@ -118,6 +119,7 @@ describe('Test proxy config', () => {
           for (const probe of probeFile.probes) {
             const Request = await proxySAM.sendRequestEvent({
               uri: probe.path,
+              headers: probe.requestHeaders,
             });
 
             if ('origin' in Request) {
