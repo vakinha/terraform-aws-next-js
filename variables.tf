0540e10b02a5150bd4813baca4bf8c2d84105308
Felix Haus <felixhaus@gmail.com>
2021-07-13 14:48:43 +0200

    Add forwarded headers

--- variables.tf
+++ variables.tf
@@ -107,13 +107,13 @@ variable "cloudfront_price_class" {
 variable "cloudfront_origin_headers" {
   description = "Header keys that should be sent to the S3 or Lambda origins. Should not contain any header that is defined via cloudfront_cache_key_headers."
   type        = list(string)
-  default     = []
+  default     = ["X-Forwarded-Host"]
 }
 
 variable "cloudfront_cache_key_headers" {
   description = "Header keys that should be used to calculate the cache key in CloudFront."
   type        = list(string)
-  default     = ["Authorization"]
+  default     = ["Authorization", "Host"]
 }
 
 variable "cloudfront_external_id" {
